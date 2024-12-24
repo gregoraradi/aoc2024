ee1c0fa1a4d7a9a66f27ddfabddc4334a1ca4661
gregor <gregor.aradi@gmx.at>
2024-12-24 12:37:54 +0100

    Finish day2 part2

--- src/days/day_02.cpp
+++ src/days/day_02.cpp
@@ -3,12 +3,29 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
-#include <string_view>
 
 namespace aoc2024 {
 
 std::uint32_t Day_02::run(std::vector<std::string> const &data) {
   return part1(data);
+}
+
+bool is_safe_report(std::vector<std::int32_t> const &levels) {
+  std::vector<std::int32_t> differences;
+  std::adjacent_difference(levels.cbegin(), levels.cend(),
+                           std::back_inserter(differences));
+
+  // remove first element since it simply copies that one.
+  differences.erase(differences.begin());
+
+  auto increased =
+      std::all_of(differences.begin(), differences.end(),
+                  [](auto item) { return (item > 0 && item <= 3); });
+  auto decreased =
+      std::all_of(differences.begin(), differences.end(),
+                  [](auto item) { return (item >= -3 && item < 0); });
+
+  return (increased || decreased);
 }
 
 std::uint32_t Day_02::part1(std::vector<std::string> const &data) {
@@ -17,7 +34,6 @@ std::uint32_t Day_02::part1(std::vector<std::string> const &data) {
   for (auto const &line : data) {
     std::cout << "line: " << line << std::endl;
     std::vector<std::int32_t> levels{};
-    std::vector<std::int32_t> differences{};
 
     std::size_t start{};
     std::size_t end{};
@@ -26,76 +42,31 @@ std::uint32_t Day_02::part1(std::vector<std::string> const &data) {
     while (start != std::string::npos) {
 
       end = line.find(' ', start);
-      auto lvl = atoi(line.substr(start, end - start).c_str());
+      auto lvl = std::atoi(line.substr(start, end - start).c_str());
       levels.emplace_back(lvl);
 
       start = line.find_first_not_of(' ', end);
     }
 
-    differences.resize(levels.size());
-    std::adjacent_difference(levels.begin(), levels.end(), differences.begin());
+    if (is_safe_report(levels)) {
+      cal_sum += 1;
+    } else {
 
-    // Verify
-    differences.erase(differences.begin());
-    for (auto it = differences.begin(); it != differences.end(); ++it) {
-      //   if (it == differences.begin()) {
-      //     continue;
-      //   }
-      std::cout << *it << " " << std::endl;
-    }
+      for (auto i = 0U; i < levels.size(); ++i) {
+        std::vector<std::int32_t> cpy_levels;
+        std::copy(levels.begin(), levels.end(), std::back_inserter(cpy_levels));
 
-    auto increased =
-        std::all_of(differences.begin(), differences.end(),
-                    [](auto item) { return (item > 0 && item <= 3); });
-    auto decreased =
-        std::all_of(differences.begin(), differences.end(),
-                    [](auto item) { return (item >= -3 && item < 0); });
-    cal_sum += (increased || decreased) ? 1 : 0;
-    // if (increased || decreased) {
-    //   std::cout << ">>safe" << std::endl;
-    // }
-
-    if (!increased && !decreased) {
-
-      auto const count_neg =
-          std::count_if(differences.begin(), differences.end(),
-                        [](auto item) { return (item < 0); });
-
-      auto const count_neg_less =
-          std::count_if(differences.begin(), differences.end(),
-                        [](auto item) { return (item < -3); });
-
-      auto const count_pos =
-          std::count_if(differences.begin(), differences.end(),
-                        [](auto item) { return (item > 0); });
-
-      auto const count_pos_high =
-          std::count_if(differences.begin(), differences.end(),
-                        [](auto item) { return (item > 3); });
-
-      auto const count_zero =
-          std::count_if(differences.begin(), differences.end(),
-                        [](auto item) { return (item == 0); });
-
-      if ((count_pos_high >= 1 && (differences.back() > 3)) ||
-          (count_neg_less >= 1 && (differences.front() < -3))) {
-        std::cout << ">>unsafe!" << std::endl;
-      } else {
-        if (count_pos > count_neg) {
-          if ((count_neg + count_zero) == 1) {
-            std::cout << ">>unsafe to safe" << std::endl;
-            ++cal_sum;
-          }
-        }
-
-        if (count_neg > count_pos) {
-          if ((count_pos + count_zero) == 1) {
-            std::cout << ">>unsafe to safe" << std::endl;
-            ++cal_sum;
-          }
+        auto it = cpy_levels.begin();
+        std::advance(it, i);
+        cpy_levels.erase(it);
+        auto const usafe_to_safe_report = is_safe_report(cpy_levels);
+        if (usafe_to_safe_report) {
+          cal_sum += 1;
+          break;
         }
       }
     }
+
     std::cout << "---" << std::endl;
   }
 
